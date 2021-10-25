9c5db6258fc38995d1ace14eea54b41f1be1d058
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-10-25 12:10:35 +0530

    Deployment
    Deployment of Project with new set of questions with minimal .css and .js changes

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "Pollen grains, mites, and feathers may be the cause of:",
+				option1: "Pneumonia",
+				option2: "Tonsillitis",
+				option3: "Allergic rhinitis",
+				option4: "Emphysema",
+				answer: 3
+			},
+			{
+				question: "Contractions of the respiratory muscles result in a(n):",
+				option1: "Decrease in the volume of the thorax",
+				option2: "Increase in the volume of the thorax",
+				option3: "Increase in the amount of blood flowing through the lungs",
+				option4: "Decrease in the amount of blood flowing through the lungs",
+				answer: 2
+			},
+			{
+				question: "Spasms of the smooth muscles in the bronchial tree may be triggered by an allergy and may result in:",
+				option1: "Asthma",
+				option2: "Hay fever",
+				option3: "Skeletal muscle contraction",
+				option4: "Contractions of the cartilage rings",
+				answer: 1
+			},
+			{
+				question: "The vital capacity is the:",
+				option1: "Volume of air left in the lungs after forceful expiration",
+				option2: "Largest volume of air that can be expelled from the lungs",
+				option3: "Amount of air remaining in the lungs after expiration",
+				option4: "Amount of air entering the lung during a normal inspiration",
+				answer: 1
+			},
+			{
+				question: "A normal inspiration of air brings into the lungs about:",
+				option1: "2500 mL of air",
+				option2: "About 10,000 mL of air",
+				option3: "About 500 mL of air",
+				option4: "About 10 mL of air",
+				answer: 3
+			}
+		]
+	];
